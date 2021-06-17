38eef9c85e6915d254b31938550e8b2f71b525ff
Clément Plantec <plantec.clement@gmail.com>
2021-06-17 13:27:15 +0200

    Create Constants.cs

---
+++ DspHelper/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace DspHelper.Helpers
+{
+    internal class Constants
+    {
+        internal static string ExecutableDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        internal static string DataFile = $"{ExecutableDirectory}/data.xml";
+    }
+}
