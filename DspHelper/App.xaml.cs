4ea063394a6432fef00f871bdd17327a0a8602fb
Clément Plantec <plantec.clement@gmail.com>
2021-06-17 13:24:10 +0200

    Implement App Services

--- DspHelper/App.xaml.cs
+++ DspHelper/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DspHelper.Models;
+using DspHelper.ViewModels;
+using DspHelper.ViewModels.ItemsCollection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +18,31 @@ namespace DspHelper
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Services = ConfigureServices();
+        }
+
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+        /// </summary>
+        public IServiceProvider Services { get; }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Viewmodels
+            services.AddSingleton<DspComponentsCollectionViewModel>();
+            services.AddSingleton<DspBuildingsCollectionViewModel>();
+            services.AddSingleton<DspSourcesCollectionViewModel>();
+
+            return services.BuildServiceProvider();
+        }
     }
 }
